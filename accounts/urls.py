3fb0e4d1b0e131a42e338cf975c563dc21275fb3
l-yoon <min>
2024-09-10 12:02:40 +0900

    first commit

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("",views.SignupView.as_view()),
-    path("login/",views.LoginView.as_view()),
-    path("logout/",views.LogoutView.as_view()),
-    path("<str:username>/",views.ProfileView.as_view()),
-]
